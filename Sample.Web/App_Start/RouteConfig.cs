c5dcc12b35e8a346968715a801c4963aed02d739
Michael <femiot@hotmail.com>
2015-04-09 20:45:28 +0200

    Added Untracked nugget packages and new class files

---
+++ Sample.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sample.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection route)
+        {
+            route.Ignore("{resource}.axd/{*pathInfo}");
+
+            route.MapRoute(
+                name: "Default",
+                url: "{Controller}/{Action}/{id}",
+                defaults: new { Controller = "Home", Action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
---
+++ Sample.Web/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Sample.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+    }
+}
